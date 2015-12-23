287a5d68b0b2e5f36b3f8cb4042674a02e11066e
3dots <abyss@3dots.ru>
2015-12-23 18:51:51 +1000

    Изменение подлючения библиотек.

--- inc/tmpl/footer.php
+++ inc/tmpl/footer.php
@@ -1,3 +1,8 @@
+<footer>
+    <div class="container">
+        просто футер
+    </div>
+</footer>
 <div id="blueimp-gallery" class="blueimp-gallery blueimp-gallery-controls modal" data-use-bootstrap-modal="false">
     <p class="description"></p>
     <div class="slides"></div>
@@ -5,6 +10,7 @@
     <a href="" class="close v-close" data-dismiss="modal" aria-label="Close"><span class="close-text">Закрыть</span></a>
 </div>
 <script src="/vendor/jquery.min.js"></script>
+<script src="/vendor/tether/js/tether.min.js"></script>
 <script src="/vendor/bootstrap.min.js"></script>
 <script src="/vendor/handlebars.js"></script>
 <script src="/vendor/jquery.validate.min.js"></script>
--- inc/tmpl/header.php
+++ inc/tmpl/header.php
@@ -1,2 +1,31 @@
 <? CJSCore::Init(array('ajax', 'window')); ?>
-
+<div class="">
+    <div class="collapse" id="navbar-header">
+        <div class="container bg-inverse p-a-1">
+            <h3>Collapsed content</h3>
+            <p>Toggleable via the navbar brand.</p>
+        </div>
+    </div>
+    <nav class="navbar navbar-light navbar-static-top bg-faded">
+        <div class="container">
+            <button class="navbar-toggler hidden-sm-up" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar2">☰</button>
+            <div class="collapse navbar-toggleable-xs" id="exCollapsingNavbar2">
+                <a class="navbar-brand" href="#">Шапка сайта</a>
+                <ul class="nav navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">пункт меню 1<span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Пункт меню 2</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Пункт меню 3</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Пункт меню 4</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</div>
